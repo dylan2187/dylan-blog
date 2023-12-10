15d7ed37c6f389b9b8d0bc317dea03063d68af45
neodylan <neodylan@foxmail.com>
2023-12-10 16:09:15 +0800

    feat: mock配置的方法

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Random, mock } from 'mockjs';
 
 type Data = {
-  name: string
-}
+	name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'dylan ari' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	res.status(200).json(
+		mock({
+			'list|100': [
+				{
+					// 属性 id 是一个自增数，起始值为 1，每次增 1
+					// 'id|+1': 1,
+					'name|1': [Random.cname(), Random.cname()],
+					createDate: Random.date('yyyy-MM-dd'),
+					address: Random.county(true),
+				},
+			],
+		})
+	);
 }
