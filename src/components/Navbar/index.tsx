7208cb50908b0ad4874e3ad9f56816c0528b4a45
neodylan <neodylan@foxmail.com>
2023-12-10 17:47:55 +0800

    feat: navbar设计

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,23 @@
 import { NextPage } from 'next';
+import styles from './index.module.scss';
+import { navs } from './config';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 const Navbar: NextPage = () => {
-	return <div>导航</div>;
+	const { pathname, push } = useRouter(); //使用这个hook拿到当前路由的信息
+
+	return (
+		<div className={styles.navbar}>
+			<section className={styles.logoArea}>dylan blog</section>
+			<section className={styles.linkArea}>
+				{navs?.map((nav) => (
+					<Link key={nav?.label} href={nav?.value} className={pathname === nav?.value ? styles.active : ''}>
+						{nav?.label}
+					</Link>
+				))}
+			</section>
+		</div>
+	);
 };
 
 export default Navbar;
